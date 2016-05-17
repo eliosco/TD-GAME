c316ea074c7374c5430710217174e3ab7a1fc2de
eliosco <eliasdjpizza@hotmail.it>
2016-05-17 19:02:14 +0200

    Update AscoltatoreDiEventiSuperiore.java

--- src/ascoltatori/AscoltatoreDiEventiSuperiore.java
+++ src/ascoltatori/AscoltatoreDiEventiSuperiore.java
@@ -12,7 +12,7 @@ import java.awt.event.*;
 import static finestre.Finestra.*;
 
 /**
- *
+ *classe che implementa le interfaccie MouseListener e MouseMotionListener che si occupano della gestione degli eventi in base alle azioni del mouse
  * @author User
  */
 public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionListener {
@@ -20,10 +20,10 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
     /**
      *
      */
-    public AscoltatoreDiEventiSuperiore() {
-        super();
-    }
-
+  //ho cancellato il costruttore 
+  
+  
+  //se il mouse viene cliccato allora la variabile "drag viene settata a -1"
     @Override
     public void mouseClicked(MouseEvent e) {
 
@@ -35,7 +35,7 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
     public void mousePressed(MouseEvent e) {
 
     }
-
+//se il mouse viene rilasciato viene confrontato il denaro del giocatore con quello della torretta in drag, se la posizione in cui viene rilasciata sia quella giusta e se il drag è diverso da -1, dopo di che, se vengono soddisfatte tutte le condizioni si attiva una delle casistitische dello switch case;
     @Override
     public void mouseReleased(MouseEvent e) {
         try {
@@ -45,7 +45,7 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
                     && panel.getGriglia().giustaposizione(e.getX(), e.getY() )
                     && panel.getDrag() != -1) {
 
-                switch (panel.getDrag()) {
+                switch (panel.getDrag()) {//in base alla torretta rilasciata si andranno a modificare le caratteristiche del giocatore con un decremento del contatore delle torrette
                     case 0:
                         panel.getTorrette().add(new Torretta1(15, e.getX() / 40, e.getY() / 40, panel.getProiettili()));
                         panel.getContTorr()[panel.getTorrette().size()-1]=0;
@@ -58,10 +58,10 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
                     break;
                 }
 
-                iPanel.update(giocatore);
+                iPanel.update(giocatore);//vengono aggiornati i dati del giocatore con la chiamata al metodo update del pannello inferiore
 
             }
-            panel.setDrag(-1);
+            panel.setDrag(-1); //settaggio a -1 della variabile "drag"
         } catch (Exception ec) {
             System.out.println("Pannello sbagliato");
         }
@@ -76,7 +76,8 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
     public void mouseExited(MouseEvent e) {
 
     }
-
+// quando il mouse è in drag scorre il ciclo for fino alla dimensione del negozio del pannello superiore e controlla se il bottone del negozio contiene il mouse e se i soldi del giocatore sono maggiori o al massimo uguali al costo della torretta contenuta dal bottone del negozio
+ // se la condizione risulta verificata setta il drag uguale alla variabile i-esima del for per poi settare la x e la y della torretta i-esima del negozio   
     @Override
     public void mouseDragged(MouseEvent e) {
 
@@ -90,7 +91,7 @@ public class AscoltatoreDiEventiSuperiore implements MouseListener, MouseMotionL
             }
         }
     }
-
+// crea un nuovo punto del mouse e lo assegna alla variabile "topo del pannello superiore"
     @Override
     public void mouseMoved(MouseEvent e) {
         sPanel.setTopo(new Point((e.getX()), e.getY()));
